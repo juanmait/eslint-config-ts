9bf5fb76b645f1b2c120c429e283f3cf6d2e5ea2
Juanma <juanmanuel@patagonian.it>
2022-05-09 16:10:19 -0300

    copy source from eslint-config-monosvelte

---
+++ README.md
@@ -0,0 +1,77 @@
+# eslint-config-ts
+
+```bash
+npm install @cubostuff/eslint-config-ts
+```
+
+You'll also need to install the `peerDependencies`.
+
+## Usage
+
+Create a file `.eslintrc.js` and add the following.
+
+> should be **.eslintrc.cjs** if your package.json has `"type": "module"`.
+
+```js
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  root: true,
+  // add this if you're in a monorepo and don't want a specific member to inherit eslint rules from
+  // other configs that exists higher on the file system's tree.
+
+  extends: '@cubostuff/eslint-config-ts',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+};
+```
+
+## Release
+
+Make sure to check this links:
+
+- https://www.conventionalcommits.org
+- https://keepachangelog.com
+- https://semver.org
+
+```bash
+npm version minor
+# options (major, minor, or patch)
+# - increments the version your package.json based on the type of the change
+# - commits this version bump
+# - creates a tag for the current release
+npm publish
+git push
+git push --tags
+```
+
+## Release Beta
+
+```bash
+npm version 1.0.0-beta.1
+# - bump package version
+# - creates a git tag
+npm publish --tag beta
+git push
+git push --tags
+```
+
+## Peer Dependencies
+
+- @types/eslint
+- @typescript-eslint/eslint-plugin
+- @typescript-eslint/parser
+- eslint
+- eslint-config-prettier
+- eslint-plugin-eslint-comments
+- eslint-plugin-import
+- prettier
+- typescript
+
+## Dependency changelogs
+
+- https://github.com/typescript-eslint/typescript-eslint/blob/main/CHANGELOG.md
+- https://github.com/eslint/eslint/blob/main/CHANGELOG.md
---
+++ index.js
@@ -0,0 +1,89 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      parserOptions: {
+        ecmaVersion: 2019,
+        sourceType: 'module',
+      },
+      extends: [
+        /**
+         * ESLint's builtin "recommended" config - it turns on a small, sensible set of rules which
+         * lint for well-known best-practices.
+         */
+        'eslint:recommended',
+        /**
+         * This plugin intends to support linting of ES2015+ (ES6+) import/export syntax, and
+         * prevent issues with misspelling of file paths and import names.
+         * For typescript it relies on `eslint-import-resolver-typescript` bellow.
+         *
+         * Check [here](https://www.npmjs.com/package/eslint-plugin-import#settings) for
+         * more settings to resolve problems with resolving imports.
+         */
+        'plugin:import/recommended',
+        /**
+         * This plugin adds TypeScript support to eslint-plugin-import.
+         *
+         * - import/require files with extension .ts/.tsx!
+         * - Use paths defined in tsconfig.json.
+         * - Prefer resolve @types/* definitions over plain .js.
+         * - Multiple tsconfigs support just like normal.
+         *
+         * See extra configs [here](https://github.com/alexgorbatchev/eslint-import-resolver-typescript#configuration)
+         */
+        'plugin:import/typescript',
+        /**
+         * is our "recommended" config - it's just like eslint:recommended, except it only turns on
+         * rules from our TypeScript-specific plugin.
+         */
+        'plugin:@typescript-eslint/recommended',
+        /**
+         * Check [Linting with Type Information](https://typescript-eslint.io/docs/linting/type-linting/).
+         * Contains rules that specifically require type information.
+         */
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        /**
+         * Apply best practices on directive comments such as `// eslint-disable`
+         * - to disallow unused disabling.
+         * - to disallow non-effect enabling.
+         * - to require rule IDs for disabling and enabling.
+         */
+        'plugin:eslint-comments/recommended',
+        /**
+         * eslint-config-prettier ensure ESLint doesn't report on formatting issues that prettier will fix.
+         * - check [here](https://typescript-eslint.io/docs/linting/linting#prettier)
+         * - check [here](https://prettier.io/docs/en/install.html#eslint-and-other-linters)
+         * - check [here](https://prettier.io/docs/en/integrating-with-linters.html)
+         */
+        'prettier',
+      ],
+    },
+    {
+      files: ['*.cjs', '*.js'],
+      env: {
+        node: true,
+        browser: false,
+        es6: true,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:import/recommended',
+        'plugin:eslint-comments/recommended',
+        'prettier',
+      ],
+    },
+  ],
+};
